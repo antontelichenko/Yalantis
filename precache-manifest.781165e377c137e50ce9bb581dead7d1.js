190d98626ffd1f06a01dbbf11422ba8839922c3e
Anton Anton <geka412315@gmail.comgit config --global user.name geka412315git config --global user.email geka412315@gmail.com>
2020-07-22 18:00:44 +0300

    Updates

---
+++ precache-manifest.781165e377c137e50ce9bb581dead7d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806acf9ee015ce49c685946085f92c4e",
+    "url": "/Yalantis/index.html"
+  },
+  {
+    "revision": "5b0ea7cdd9b51ae52af9",
+    "url": "/Yalantis/static/css/main.51c63e65.chunk.css"
+  },
+  {
+    "revision": "d59efd5e84e7808f0d9d",
+    "url": "/Yalantis/static/js/2.b7b78004.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Yalantis/static/js/2.b7b78004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0ea7cdd9b51ae52af9",
+    "url": "/Yalantis/static/js/main.48fb527c.chunk.js"
+  },
+  {
+    "revision": "ce9939868b84cedaac31",
+    "url": "/Yalantis/static/js/runtime-main.819324d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Yalantis/static/media/loader.5d5d9eef.svg"
+  }
+]);
